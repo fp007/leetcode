b8242363da5d2d7d7b674089e1bda5e9e32a6fcc
fp007 <55970449+fp007@users.noreply.github.com>
2021-02-01 17:39:32 +0800

    Create 1.two_sum.py

---
+++ 1.two_sum.py
@@ -0,0 +1,3 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        return (d := {}) or next(([d[num], i] for i, num in enumerate(nums) if num in d or d.setdefault(target - num, i) < 0))
